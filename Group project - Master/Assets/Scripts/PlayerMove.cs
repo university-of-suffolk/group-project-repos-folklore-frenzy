12a97dc28a794a4fa99eeba38565462978697e07
SamBrades <131149300+SamBrades1@users.noreply.github.com>
2023-06-01 15:18:45 +0100

    Reintroduced Directional Rebound
    Makes crashing less time consuming.

--- Group project - Master/Assets/Scripts/PlayerMove.cs
+++ Group project - Master/Assets/Scripts/PlayerMove.cs
@@ -11,6 +11,8 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] LayerMask building;
     [SerializeField] LayerMask ramp;
     bool hitBuilding;
+    bool distanceFromGround;
+    [SerializeField] LayerMask ground;
 
     [Header("Speed Controls")]
     [HideInInspector] public float Speed;
@@ -30,9 +32,9 @@ public class PlayerMove : MonoBehaviour
     [Header("Turn controls")]
     [SerializeField] float turnSens = 10f;
     [HideInInspector] public Vector3 MovementDirection;
-    [SerializeField] float rampCheckOffset;
-    [SerializeField] float rampCheckdistance;
-    [SerializeField] bool isRamp;
+    //[SerializeField] float rampCheckOffset;
+    //[SerializeField] float rampCheckdistance;
+    //[SerializeField] bool isRamp;
 
     [Header("Input")]
     float Horizontal;
@@ -57,6 +59,26 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // if its flying off, go back to the ground
+        if (!(distanceFromGround = Physics.Raycast(transform.position, Vector3.down, 20f, ground)))
+        {
+            Debug.Log("Too high");
+            rb.angularVelocity = Vector3.zero;
+            transform.eulerAngles = new Vector3(0, transform.rotation.y, 0);
+
+            transform.position = new Vector3(41, 11, 41);
+        }
+        // if stuck in over rotation, reset to default
+        if (Mathf.Abs(transform.rotation.x) > 45 || Mathf.Abs(transform.rotation.z) > 45)
+        {
+            Debug.Log("Too rotated");
+            rb.angularVelocity = Vector3.zero;
+            transform.eulerAngles = new Vector3(0, transform.rotation.y, 0);
+
+            transform.position = new Vector3(41, 11, 41);
+        }
+
+
         // Only get the player input when the game is not paused. (I.e., the player can't move while paused)
         if (Time.timeScale == 1f)
         {
@@ -110,8 +132,15 @@ public class PlayerMove : MonoBehaviour
             // change the velocity to be in the direction of travel so it wont drift
             if (!Input.GetKey(driftKey))
             {
+                Debug.Log("Not Drifting");
+                rb.angularDrag = 0f;
                 Vector3 newVelocity = rb.velocity.magnitude * transform.forward;
                 rb.velocity = new Vector3(newVelocity.x, rb.velocity.y, newVelocity.z);
+            }
+            else
+            {
+                Debug.Log("Drifting");
+                rb.angularDrag = 300;
             }
         }
 
@@ -136,10 +165,10 @@ public class PlayerMove : MonoBehaviour
             freezeTurn = true;
             vignetteAnim.SetTrigger("Fade"); // Displays red vignette on damage
             rb.drag = 0f;
-            Speed = 5; // lower speed to give the player a chance to correct their mistake without bouncing them off the same wall repeatedly.
+            Speed = 15; // lower speed to give the player a chance to correct their mistake without bouncing them off the same wall repeatedly.
 
             //rb.velocity = Vector3.zero;
-            rb.AddForce( reboundDirection.normalized * pushbackForce * 500f * Time.fixedDeltaTime, ForceMode.Force); // applies force backwards to get players unstuck.
+            rb.AddForce( reboundDirection.normalized * pushbackForce * /*500f **/ Time.fixedDeltaTime, ForceMode.Impulse); // applies force backwards to get players unstuck.
 
             // Decrease score on collision.
             if (!scoreChanged)
@@ -162,8 +191,8 @@ public class PlayerMove : MonoBehaviour
         scoreChanged = false; // Player will lose money again on next collision.
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
+    public void OnCollisionEnter(Collision collision)
+    { 
         if (collision.gameObject.CompareTag("Building"))
         {
             Debug.Log("Collide with obstacle");
@@ -171,8 +200,14 @@ public class PlayerMove : MonoBehaviour
             rb.constraints = RigidbodyConstraints.FreezeRotationY;
             rb.constraints = RigidbodyConstraints.FreezeRotationX;
             rb.constraints = RigidbodyConstraints.FreezeRotationZ;
+            // set velocity or do the rebound addforce on the rb
+            reboundDirection = /*Vector3.Reflect(rb.velocity,*/(rb.velocity/4) + collision.contacts[0].normal/*)*/;
 
-            reboundDirection = transform.forward * -1;
+            rb.velocity = reboundDirection;
+
+            //Debug.Log("collisionPoint: " + collisionPoint);
+
+            //reboundDirection = /*collisionPoint - transform.position*/ transform.forward * -1;
             //reboundDirection.y = 0f;
             hitBuilding = true;
 
@@ -181,9 +216,9 @@ public class PlayerMove : MonoBehaviour
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red;
+        //Gizmos.color = Color.red;
 
-        Gizmos.DrawLine(transform.position + Vector3.down * rampCheckOffset, transform.position + Vector3.down * rampCheckOffset + transform.forward * rampCheckdistance);
+        //Gizmos.DrawLine(transform.position + Vector3.down * rampCheckOffset, transform.position + Vector3.down * rampCheckOffset + transform.forward * rampCheckdistance);
 
         //Gizmos.DrawLine(transform.position + frontLeftOffset, transform.position + frontLeftOffset + transform.forward * collisionRange);
         //Gizmos.DrawLine(transform.position + frontMiddleOffset, transform.position + frontMiddleOffset + transform.forward * collisionRange);
